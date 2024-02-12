877b8c1184fa403b103df6aa1c0b84e911637f32
khaledabdelwahabgit <pop.abdelwahab@gmail.com>
2024-02-12 19:08:19 +0200

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String title;
+  final String subtitle;
+  final int color;
+  final String date;
+
+  NoteModel({
+    required this.title,
+    required this.subtitle,
+    required this.color,
+    required this.date,
+  });
+}
